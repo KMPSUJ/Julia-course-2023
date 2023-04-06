fc976ce3c4d11d6fd82ef727a0640d9a15a440c1
Bartosz Zbik <bartosz@zbik.org>
2023-04-06 16:59:12 +0200

    more plotting examples

--- 04-Plotting.jl
+++ 04-Plotting.jl
@@ -1,31 +1,53 @@
 #=
 ------------------------------------------------------------
-                    Pllotting
+                    Plotting
 ------------------------------------------------------------
+This section assumes the usage of Plots.jl.
+There are other packages like Makie.jl which may be
+as good or even better (just pick one and use it).
+
+Generally you can use plot and plot! for anything.
+Plots.jl uses magic keyword arguments to costomize your plot.
 =#
 
-# get some data
-x = range(0, 1, length=20)
+using Plots
+# Plots are not showing by default, so change it
+# or invoke gui()
+# or pass show=true to the plot function
+default(:show, true)
+
+# get some random data
+x = range(-1, 1, length=20)
 y = rand(20)
 
-# function to plot anything
+# function to plot anything: plot
 plot(x, y)
+plot(sin)
+plot(sin, 2π, 4π)
+plot(x, exp)
 
-# other options can be passed
-plot(x, y, label="some data", title="First plot")
+# draw something different than a line
+plot(x, y, seriestype=:scatter)
+plot(randn(10_000), seriestype=:histogram)
+plot(x, y, st=:sticks)
 
-# specify haw to plot
-plot(x, y, st=:scatter)
+# 3d data
+plot(x, x, (t, s) -> exp(-t^2 - s^2), st=:surface)
+plot(randn(10_000), randn(10_000), st=:histogram2d)
 
-# you can use an alias
+# you can use an alias instaed of seriestype
 scatter(x, y)
+histogram(randn(10_000))
 
-# use ! functions to add something to the plot
-plot!(x, y .+ 1, st=scatter)
+# other options can be passed as optional arguments
+plot(x, y, label="some data", title="First plot", lc=:red, mc=:blue, mark=:square)
 
-# other functions usful to edit an existing plot
-title!("New title")
-xlabel("x values")
+# plot! or generally "!" is used to modify the plot
+plot!(cos)
+plot!(title="Title with plot!")
+title!("Title with title!")
+xlabel!("x axis")
+ylabel!("y axis")
 
-# save figure
+# save figure (different formats available)
 savefig("someplot.png")
